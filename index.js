63d28a830d6dee19cb7842a82a7680270ae14cd8
patankhankingwebtec <patankhanjobmail@gmail.com>
2016-04-14 19:19:34 +1000

    index.js changed

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname+'/public'));
+
 var ipaddress=process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1';
 var port=process.env.OPENSHIFT_NODEJS_PORT||3000;
 
 app.listen(port,ipaddress);
-
-app.use(express.static(__dirname+'/public'));
-app.get('/', function (req, res) {
-  res.send('Assalamu alaikum WRWB!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
